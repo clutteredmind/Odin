e4b2494f02de278d84c1ab7cfc23d953fbb98d62
ClutteredMind <yutaka@clutteredmind.com>
2018-03-05 16:42:29 -0800

    Fixed a broken require statement.

--- message-types.js
+++ message-types.js
@@ -8,7 +8,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const request = require('request-promise');
+const request = require('request');
 
 const odinUtil = require('./odin-util');
 
